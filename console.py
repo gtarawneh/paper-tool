3f1be1807c9f7429a216cb434dd8aa9927af8c49
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-08-17 02:01:58 +0100

    Move console code to new class

---
+++ console.py
@@ -0,0 +1,129 @@
+
+import curses
+import math
+
+class Console:
+
+	query = 'motion vision'
+	cache = {}
+	suggestions = []
+	keys = []
+	searchedLines = 0
+	selected = 0
+	page = 0
+	pages = 0
+	absSelected = 0
+	W = 0
+	H = 0
+	scr = None
+
+	def init(self):
+		self.scr = curses.initscr()
+		curses.start_color()
+		curses.noecho()
+		curses.cbreak
+		self.scr.keypad(1)
+		curses.use_default_colors()
+		curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+		curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+		curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
+
+	def deinit(self):
+		curses.nocbreak()
+		self.scr.keypad(0)
+		curses.echo()
+		curses.endwin()
+
+	def clearLine(self, i):
+		reps = self.W-1 if (i == self.H-1) else self.W
+		self.scr.addstr(i, 0, ' ' * reps)
+
+	def writeLine(self, i, str, style):
+		self.clearLine(i)
+		self.scr.addstr(i, 0, str, style)
+
+	def displaySuggestions(self, content, keys):
+		for i in self.suggestionLines:
+			self.clearLine(i)
+		startIndex = len(self.suggestionLines) * self.page
+		for i, sug in enumerate(self.suggestions[startIndex:]):
+			if i not in self.suggestionLines:
+				break
+			sug = content[sug][0:self.W]
+			self.scr.addstr(i, 0, sug)
+			for keyword in keys:
+				k = sug.lower().find(keyword.lower())
+				if k > -1:
+					self.scr.addstr(i, k, keyword, curses.color_pair(1) + curses.A_BOLD)
+
+	def highlightSuggestion(self, content):
+		if self.suggestions:
+			absSelected = len(self.suggestionLines) * self.page + self.selected
+			self.clearLine(self.selected)
+			line = content[self.suggestions[self.absSelected]][0:self.W]
+			self.scr.addstr(self.selected, 0, line, curses.color_pair(3))
+
+	def loopConsole(self, content, getSuggestionFunc):
+		queryStyle = curses.color_pair(2) + curses.A_BOLD
+		statusStyle = curses.color_pair(2) + curses.A_BOLD
+		initScreen = True
+		self.absSelected = 0
+		# main loop
+		while True:
+			if initScreen:
+				self.H, self.W = self.scr.getmaxyx()
+				self.suggestionLines = range(0, self.H - 5)
+				queryLine = self.H - 1
+				statusLine1 = self.H - 4
+				statusLine2 = self.H - 3
+				maxSuggestions = len(self.suggestionLines)
+				initScreen = False
+				# work out new page and selected values of current highligh
+				self.page = int(math.floor(float(self.absSelected) / len(self.suggestionLines)))
+				self.selected = self.absSelected % len(self.suggestionLines)
+			# update display content:
+			self.scr.refresh()
+			self.displaySuggestions(content, self.keys)
+			self.pages = math.ceil(float(len(self.suggestions)) / len(self.suggestionLines))
+			self.absSelected = len(self.suggestionLines) * self.page + self.selected
+			self.writeLine(statusLine1, "(%d / %d) [%d / %d]" % (self.absSelected + 1, len(self.suggestions), self.page + 1, self.pages), statusStyle)
+			self.writeLine(statusLine2, 'keyword: ' + ",".join(self.keys), statusStyle)
+			self.writeLine(queryLine, '> ' + self.query, queryStyle)
+			self.highlightSuggestion(content)
+			# grab and process input:
+			c = self.scr.getch()
+			if c == 10:
+				return
+			elif c == curses.KEY_RESIZE:
+				initScreen = True
+			elif c == curses.KEY_DOWN:
+				if self.absSelected < len(self.suggestions)-1:
+					self.absSelected += 1
+					initScreen = True
+			elif c == curses.KEY_UP:
+				if self.absSelected > 0:
+					self.absSelected -= 1
+					initScreen = True
+			elif c == curses.KEY_NPAGE:
+				if self.page < self.pages-1:
+					self.absSelected += len(self.suggestionLines)
+					self.absSelected = min(self.absSelected, len(self.suggestions)-1)
+					initScreen = True
+			elif c == curses.KEY_END:
+				prevLines = len(self.suggestionLines) * self.page
+				remainingLines = len(self.suggestions) - prevLines
+				self.selected = min(len(self.suggestionLines)-1, remainingLines-1)
+			elif c == curses.KEY_HOME:
+				self.selected = 0
+			elif c == curses.KEY_PPAGE:
+				if self.page > 0:
+					self.absSelected -= len(self.suggestionLines)
+					initScreen = True
+			else:
+				self.query = self.query[:-1] if (c == 127) else self.query + unichr(c)
+				# run query:
+				if len(self.query) > 4:
+					self.keys = self.query.split(' ')
+					subList = self.cache.get(self.query[:-1], range(0, len(content)))
+					self.suggestions = getSuggestionFunc(content, subList, self.keys, 1000000)
+					self.cache[self.query] = self.suggestions
--- papertool.py
+++ papertool.py
@@ -2,8 +2,9 @@
 
 import sys
 import traceback
-import curses
 import math
+
+from console import *
 
 filename = 'sentences.txt'
 
@@ -11,111 +12,6 @@ def getContent():
 	with open(filename) as f:
 		content = f.read().splitlines()
 		return content
-
-def runScreen(content, scr):
-	curses.use_default_colors()
-	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-	curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
-	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
-	query = 'motion vision'
-	cache = {}
-	suggestions = []
-	keys = []
-	searchedLines = 0
-	selected = 0
-	page = 0
-	pages = 0
-	absSelected = 0
-	queryStyle = curses.color_pair(2) + curses.A_BOLD
-	statusStyle = curses.color_pair(2) + curses.A_BOLD
-	initScreen = True
-	# inline functions
-	def clearLine(i): scr.addstr(i, 0, ' ' * (W))
-	def clearQueryLine(): scr.addstr(queryLine, 0, ' ' * (W-1))
-	def writeLine(i, str, style):
-		if (i == queryLine):
-			clearQueryLine()
-		else:
-			clearLine(i)
-		scr.addstr(i, 0, str, style)
-	def displaySuggestions():
-		for i in suggestionLines:
-			clearLine(i)
-		startIndex = len(suggestionLines) * page
-		for i, sug in enumerate(suggestions[startIndex:]):
-			if i not in suggestionLines:
-				break
-			sug = content[sug][0:W]
-			scr.addstr(i, 0, sug)
-			for keyword in keys:
-				k = sug.lower().find(keyword.lower())
-				if k > -1:
-					scr.addstr(i, k, keyword, curses.color_pair(1) + curses.A_BOLD)
-	def highlightSuggestion():
-		if suggestions:
-			absSelected = len(suggestionLines) * page + selected
-			clearLine(selected)
-			line = content[suggestions[absSelected]][0:W]
-			scr.addstr(selected, 0, line, curses.color_pair(3))
-	# main loop
-	while True:
-		if initScreen:
-			H, W = scr.getmaxyx()
-			suggestionLines = range(0, H - 5)
-			queryLine = H - 1
-			statusLine1 = H - 4
-			statusLine2 = H - 3
-			maxSuggestions = len(suggestionLines)
-			initScreen = False
-			# work out new page and selected values of current highligh
-			page = int(math.floor(float(absSelected) / len(suggestionLines)))
-			selected = absSelected % len(suggestionLines)
-		# update display content:
-		scr.refresh()
-		displaySuggestions()
-		pages = math.ceil(float(len(suggestions)) / len(suggestionLines))
-		absSelected = len(suggestionLines) * page + selected
-		writeLine(statusLine1, "(%d / %d) [%d / %d]" % (absSelected + 1, len(suggestions), page + 1, pages), statusStyle)
-		writeLine(statusLine2, 'keyword: ' + ",".join(keys), statusStyle)
-		writeLine(queryLine, '> ' + query, queryStyle)
-		highlightSuggestion()
-		# grab and process input:
-		c = scr.getch()
-		if c == 10:
-			return
-		elif c == curses.KEY_RESIZE:
-			initScreen = True
-		elif c == curses.KEY_DOWN:
-			if absSelected < len(suggestions)-1:
-				absSelected += 1
-				initScreen = True
-		elif c == curses.KEY_UP:
-			if absSelected > 0:
-				absSelected -= 1
-				initScreen = True
-		elif c == curses.KEY_NPAGE:
-			if page < pages-1:
-				absSelected += len(suggestionLines)
-				absSelected = min(absSelected, len(suggestions)-1)
-				initScreen = True
-		elif c == curses.KEY_END:
-			prevLines = len(suggestionLines) * page
-			remainingLines = len(suggestions) - prevLines
-			selected = min(len(suggestionLines)-1, remainingLines-1)
-		elif c == curses.KEY_HOME:
-			selected = 0
-		elif c == curses.KEY_PPAGE:
-			if page > 0:
-				absSelected -= len(suggestionLines)
-				initScreen = True
-		else:
-			query = query[:-1] if (c == 127) else query + unichr(c)
-			# run query:
-			if len(query) > 4:
-				keys = query.split(' ')
-				subList = cache.get(query[:-1], range(0, len(content)))
-				suggestions, searchedLines = getSuggestions(content, subList, keys, 1000000)
-				cache[query] = suggestions
 
 def getSuggestions(content, subList, keys, maxCount):
 	results = []
@@ -129,32 +25,19 @@ def getSuggestions(content, subList, keys, maxCount):
 			results.append(i)
 			if len(results) >= maxCount:
 				return (results, searchedLines)
-	return (results, searchedLines)
+	return results
 
-def printSuggestions(content, key):
-	results = getSuggestions(content, key, 1000)
-	for s in results:
-		print(s)
-
-def main2():
+def main():
 	content =  getContent()
-	scr = curses.initscr()
-	curses.start_color()
-	curses.noecho()
-	curses.cbreak
-	scr.keypad(1)
+	getSuggestionFunc = getSuggestions
+	con1 = Console()
+	con1.init()
 	try:
-		runScreen(content, scr)
+		con1.loopConsole(content, getSuggestionFunc)
 	except Exception as err:
-		curses.nocbreak()
-		scr.keypad(0)
-		curses.echo()
-		curses.endwin()
+		con1.deinit()
 		traceback.print_exc(file=sys.stdout)
 		return
-	curses.nocbreak()
-	scr.keypad(0)
-	curses.echo()
-	curses.endwin()
+	con1.deinit()
 
-main2()
+main()
