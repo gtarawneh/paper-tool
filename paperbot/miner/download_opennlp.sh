2e4da7878247d1eda2d02884a3455febf147e0d8
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-09-25 23:43:53 +0100

    Add script to download opennlp

---
+++ paperbot/miner/download_opennlp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE="apache-opennlp-1.6.0-bin.tar.gz"
+
+curl -Os http://mirror.catn.com/pub/apache/opennlp/$FILE
+
+tar -xzf ./$FILE
+
+JAR=`find -name 'opennlp-tools-1.6.0.jar'`
+
+mv -u $JAR .
+
+rm $FILE
