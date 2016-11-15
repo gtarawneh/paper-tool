85f97090203e28d650bcec7058196d3b51166284
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-11-15 11:36:09 +0000

    Refactor paperbot

--- paperbot.py
+++ paperbot.py
@@ -33,47 +33,51 @@ def readJSON(file):
 def getDateTimeStamp():
 	return str(datetime.datetime.now())
 
+def getFileHash(libDir):
+	dic = {}
+	for root, dirs, files in os.walk(libDir):
+		for file in files:
+			fullFile = os.path.abspath(os.path.join(root, file))
+			relFile = os.path.relpath(fullFile, libDir)
+			dic[relFile] = getSHA256(fullFile)
+	return dic
+
 def main():
 	homeDir = os.getenv("HOME")
 	libDir = getAbsolutePath(homeDir, "pdfs")
 	libFile = getAbsolutePath(homeDir, "pdfs.json")
+	fileHash = getFileHash(libDir)
 	dic = readJSON(libFile)
-	hmap = {entry["sha256"]:entry for entry in dic}
+	hmap = {entry["sha256"]:entry for entry in dic} # sha256 -> dic entry
 	changes = False
 	# first, loop through files in the lib directory
 	fileList = []
-	for root, dirs, files in os.walk(libDir):
-		path = root.split('/')
-		for file in files:
-			fullFile = os.path.abspath(os.path.join(root, file))
-			relFile = os.path.relpath(fullFile, libDir)
-			fileHash = getSHA256(fullFile)
-			fileList.append(relFile)
-			existingEntry = hmap.get(fileHash, None)
-			if existingEntry:
-				if relFile != existingEntry["file"]:
-					print("file moved: %s -> %s" % (existingEntry["file"], relFile))
-					existingEntry["file"] = relFile
-					existingEntry["added"] = getDateTimeStamp()
-					changes = True
-			else:
-				print("found new file %s" % relFile)
-				dic.append({
-					"file": relFile,
-					"sha256": fileHash,
-					"added": getDateTimeStamp(),
-				});
+	for relFile, fileHash in fileHash.iteritems():
+		fullFile = os.path.join(libDir, relFile)
+		fileList.append(relFile)
+		existingEntry = hmap.get(fileHash, None)
+		if existingEntry:
+			if relFile != existingEntry["file"]:
+				print("file moved: %s -> %s" % (existingEntry["file"], relFile))
+				existingEntry["file"] = relFile
+				existingEntry["added"] = getDateTimeStamp()
 				changes = True
-	# check for deleted file
+		else:
+			print("found new file %s" % relFile)
+			dic.append({
+				"file": relFile,
+				"sha256": fileHash,
+				"added": getDateTimeStamp(),
+			});
+			changes = True
+	# check for deleted files
 	delFiles = [entry for entry in dic if entry["file"] not in fileList]
 	for entry in delFiles:
 		print("file deleted: %s" % entry["file"])
-		changes = True
 		dic.remove(entry)
+		changes = True
 	if changes:
 		writeJSON(libFile, dic)
-	else:
-		print("no changes detected")
 	print(json.dumps(dic, indent=4))
 
 main()
