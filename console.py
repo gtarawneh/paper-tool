gtarawneh/paper-tool

commit ce406b776095edfd29d4b98429100247990515f0

Fix issue with ctrl-p

console.py
@@ -238,7 +238,8 @@ class Console:
 				self.displayWebPage(url)
 			elif c == 16:
 				# ctrl-p
-				file = self.searcher.getFile(self.selected)
+				papInd = self.searcher.getPaperIndex(self.selected)
+				file = self.searcher.getFile(papInd)
 				pdfDir = self.getAbsolutePath(self.libDir, "pdfs")
 				self.displayPDF(file, pdfDir)
 			elif c == 2:

commit e1f7b79cf7369711d34b42652527c3e27c6b32ba

Fix ctrl-p issue introduced in ce406b7

console.py
@@ -239,7 +239,7 @@ class Console:
 			elif c == 16:
 				# ctrl-p
 				papInd = self.searcher.getPaperIndex(self.selected)
-				file = self.searcher.getFile(papInd)
+				file = self.searcher.getFile(self.selected)
 				pdfDir = self.getAbsolutePath(self.libDir, "pdfs")
 				self.displayPDF(file, pdfDir)
 			elif c == 2: